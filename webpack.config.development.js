0897a7dd0f3c2dd8f385b071793b2fa7647a34c5
Mohnish Landge <tutume1999@gmail.com>
2021-09-03 23:05:01 +0530

    Up

---
+++ webpack.config.development.js
@@ -0,0 +1,20 @@
+const merge = require("webpack-merge");
+const path = require("path");
+
+const config = require("./webpack.config");
+
+module.exports = merge(config, {
+  mode: "development",
+
+  devtool: "inline-source-map",
+
+  devServer: {
+    writeToDisk: true,
+    host: "0.0.0.0",
+    useLocalIp: true,
+  },
+
+  output: {
+    path: path.join(__dirname, "InifniteAutoScrollingGallery"),
+  },
+});
